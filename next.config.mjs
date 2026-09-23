5d30650c72f1629781cb9e4bd8d73d38d310fc58
androidDeveloperOne <android.developer@knestaluform.in>
2026-09-23 16:23:54 +0530

    Add Next.js GitHub Pages CI/CD

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,11 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+    output: "export",
+    basePath:"sanviktechnologies",
+      images: {
+    unoptimized: true,
+  },
 }
 
 export default nextConfig
