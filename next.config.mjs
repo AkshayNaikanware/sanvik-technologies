756dc0d36b7e3d62814ee448015288922cf5b0a7
androidDeveloperOne <android.developer@knestaluform.in>
2026-09-23 16:27:42 +0530

    Fix Next.js base path

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
     unoptimized: true,
   },
     output: "export",
-    basePath:"sanviktechnologies",
+    basePath:"/sanviktechnologies",
       images: {
     unoptimized: true,
   },
